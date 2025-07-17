546467752b2c9d2b09e8194e93313efd7ec092d5
laxxxx <lakshmancool9@gmail.com>
2025-07-17 23:14:56 +0530

    Initial Commit

---
+++ SayMyName-BB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SayMyName-BB
+//
+//  Created by Sree Lakshman on 17/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SayMyName-BB/SayMyName_BBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayMyName_BBApp.swift
+//  SayMyName-BB
+//
+//  Created by Sree Lakshman on 17/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SayMyName_BBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
